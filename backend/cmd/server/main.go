1ced153cfdfc29bbd8f64124d6c1823ca776d47f
Muhammad-Ahmad94 <muneebbum757@gmail.com>
2025-05-24 21:07:13 +0500

    created simple go server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Future Capsule API running")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Sever is running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
